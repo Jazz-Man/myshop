9397d7fe6a583279db66af3ea253630d72239663
Jazz <vsokolyk@gmail.com>
2017-08-03 22:50:48 +0300

    setup session to database

--- app/Providers/LocalEnvironmentServiceProvider.php
+++ app/Providers/LocalEnvironmentServiceProvider.php
@@ -2,10 +2,24 @@
 
 namespace App\Providers;
 
+use Illuminate\Foundation\AliasLoader;
 use Illuminate\Support\ServiceProvider;
 
 class LocalEnvironmentServiceProvider extends ServiceProvider
 {
+    
+    protected $providers
+      = [
+        \Barryvdh\Debugbar\ServiceProvider::class,
+        \Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class,
+//        \Laracasts\Generators\GeneratorsServiceProvider::class,
+//        \Orangehill\Iseed\IseedServiceProvider::class
+      ];
+    
+    protected $aliases
+      = [
+        'Debugbar' => 'Barryvdh\Debugbar\Facade',
+      ];
     /**
      * Bootstrap the application services.
      *
@@ -13,7 +27,23 @@ class LocalEnvironmentServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        if ( $this->app->environment() === 'local' ) {
+            $this->registerServiceProviders();
+            $this->registerFacadeAliases();
+        }
+    }
+    
+    protected function registerServiceProviders() {
+        foreach ( $this->providers as $provider ) {
+            $this->app->register( $provider );
+        }
+    }
+    
+    public function registerFacadeAliases() {
+        $loader = AliasLoader::getInstance();
+        foreach ( $this->aliases as $alias => $facade ) {
+            $loader->alias( $alias, $facade );
+        }
     }
 
     /**
